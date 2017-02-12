0e8eb6be294f77c2415414aa4a4d4d52e75a09d0
gauravpai <gmp.paido@gmail.com>
2017-02-12 15:21:04 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -7,7 +7,7 @@ g = open(path, 'r')
 h = open(path1, 'w')
 
 for line in g:
-    if re.match("(.*)html(.*)", line):
+    if re.match("(.*)article(.*)", line):
         if re.match("(.*)archive(.*)", line):
             continue
         if re.match("(.*)book(.*)", line):
